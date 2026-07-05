f6049520b815292d036da77864e925da0bdab8b1
samundeeswari25 <samusmart25@gmail.com>
2026-07-05 07:37:23 +0530

    playwright-Framework

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
 
     use: {
         headless: true,
+
         screenshot: 'only-on-failure',
         video: 'retain-on-failure',
         trace: 'retain-on-failure'
--- src/fixtures/testFixture.ts
+++ src/fixtures/testFixture.ts
@@ -1,14 +1,20 @@
 import { test as base } from '@playwright/test';
-import { PageObjectManager } from '../PageObjects/PageObjectManager';
+import { LoginPage } from '../pages/loginPage'
 
 type MyFixtures = {
-    pom: PageObjectManager;
+    login:LoginPage
 };
 
+// export const test1 = base.extend<MyFixtures>({
+//     pom: async ({ page }, use) => {
+//         const pom = new PageObjectManager(page);
+//         await use(pom);
+//     }
+// });
 export const test = base.extend<MyFixtures>({
-    pom: async ({ page }, use) => {
-        const pom = new PageObjectManager(page);
-        await use(pom);
+    login: async ({ page }, use) => {
+        const login = new LoginPage(page);
+        await use(login);
     }
 });
 
